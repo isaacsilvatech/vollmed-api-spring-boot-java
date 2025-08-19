b427a0699ad11fe15475eefec73b90d8c2f29e98
IsaacSilva <isaacsilva.info@gmail.com>
2025-08-18 22:48:08 -0300

    adicionado linha

--- src/main/java/med/voll/api/config/SecurityConfig.java
+++ src/main/java/med/voll/api/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
         return authentication.getAuthenticationManager();
     }
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
